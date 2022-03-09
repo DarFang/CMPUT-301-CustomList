742581f947eded9ed0230df89b3d518d4c823633
Darius Fang <56571687+DarFang@users.noreply.github.com>
2022-03-09 15:40:51 -0700

    fixed gradle part 2

--- app/src/test/java/CustomListTest.java
+++ app/src/test/java/CustomListTest.java
@@ -1,23 +1,23 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
 
 import com.example.simpleparadox.listycity.City;
 import com.example.simpleparadox.listycity.CustomList;
 
-
-import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 public class CustomListTest {
     private CustomList list;
-
+    @Before
     public void createList(){
         list = new CustomList(null, new ArrayList<City>());
-    }
+        }
     @Test
     public void addCityTest(){
-        createList();
         int listSize = list.getCount();
         list.addCity(new City("Edmonton", "AB"));
         assertEquals(list.getCount(), listSize+1);
